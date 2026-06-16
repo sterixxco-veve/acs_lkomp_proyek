9866e8dfff0914e2dcf65f211826dd5037ba5885
sterixxco-veve <anastasiaevelyn11@gmail.com>
2026-06-16 14:19:39 +0700

    push hosting db

--- src/main/db_handlers.js
+++ src/main/db_handlers.js
@@ -6,10 +6,11 @@ import bcrypt from 'bcryptjs'
 // ========================================
 
 const pool = mysql.createPool({
-  host: 'localhost',
-  user: 'lkomp_admin',
-  password: 'admin_password',
+  host: 'mysql-acslkomp-acslkomp.e.aivencloud.com',
+  user: 'avnadmin',
+  password: 'AVNS__tyiILILrwSe_0x-Uf3',
   database: 'acs_lkomp',
+  port: 22528,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
